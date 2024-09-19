6cde954f5cbf0a00f9ef089c12fe42b02676d9a1
andromeda <q.a.r.h.hrp0204@gmail.com>
2024-09-19 21:25:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const texts = [
+    "Web Developer",
+    "UI UX Designer",
+    "Content Creator",
+]
+
+let speed = 100;
+
+const textElement = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let characterIndex = 0;
+
+function typeWriter(){
+    if(characterIndex < texts[textIndex].length){
+        textElement.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter,speed)
+    }
+
+    else{
+        setTimeout(eraseText,1000)
+    }
+}
+
+function eraseText(){
+    if(textElement.innerHTML.length > 0){
+        textElement.innerHTML = textElement.innerHTML.slice(0,-1)
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1)%texts.length;
+        characterIndex = 0;
+        setTimeout(typeWriter,500)
+    }
+}
+
+window.onload= typeWriter
